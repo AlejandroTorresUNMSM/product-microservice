7b341a7d202830e16bf4aa0b7e2285b40602c53c
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-10 14:28:33 -0500

    Quitando producto y dejando account y credit

--- src/main/java/com/atorres/nttdata/productomicroservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atorres.nttdata.productomicroservice.repository;
-
-import com.atorres.nttdata.productomicroservice.model.dao.ProductDao;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends ReactiveMongoRepository<ProductDao,String> {
-}
